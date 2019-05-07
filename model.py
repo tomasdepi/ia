027f3ecd78fe5978eacd25b2f6c48c2ecb429453
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2019-05-06 22:02:38 -0300

    using utilities and constants

--- model.py
+++ model.py
@@ -1,23 +1,15 @@
 import pandas as pd
+import utilities
+import constants
 from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import UnsupervisedDataSet  
 from pybrain.structure.modules import TanhLayer
 
-MAX_WORD_LENGHT = 20
-
-def wordToList(word):
-
-	lenWord = len(word)
-	listOfCeros = [0] * (20-lenWord)
-	l = [ord(character) for character in word]
-
-	return l + listOfCeros
-
 print('[+] Loading data set')
 
-dataset = SupervisedDataSet(MAX_WORD_LENGHT, 1)
+dataset = SupervisedDataSet(constants.MAX_WORD_LENGHT, 1)
 
 dataframe = pd.read_csv('finalDataSetTrainingWithCharacters.csv')
 dataframe.drop(columns=['Word', 'Len'], inplace=True)
@@ -32,7 +24,7 @@ for _, row in dataframe.iterrows():
 print('[+] Dataset loaded successfully')
 print('[+] Starting Tranining.....')
 
-net = buildNetwork(MAX_WORD_LENGHT, 10, 1, bias=True, hiddenclass=TanhLayer) # cant input nodes, hidden nodes nd output nodes
+net = buildNetwork(constants.MAX_WORD_LENGHT, 10, 1, bias=True, hiddenclass=TanhLayer) # cant input nodes, hidden nodes nd output nodes
 trainer = BackpropTrainer(net, dataset)
 trainer.trainUntilConvergence()
 
@@ -43,13 +35,13 @@ while 1:
 	userInput = input("ingrese palabra:")
 
 	if len(userInput) > MAX_WORD_LENGHT:
-		print("[-] Error: The word exced {maxCharacters} characters".format(maxCharacters=MAX_WORD_LENGHT))
+		print("[-] Error: The word exced {maxCharacters} characters".format(maxCharacters=constants.MAX_WORD_LENGHT))
 		continue
 
-	netInput = wordToList(userInput)
+	netInput = utilities.wordToList(userInput)
 	print(netInput)
 
-	dst = UnsupervisedDataSet(MAX_WORD_LENGHT, )
+	dst = UnsupervisedDataSet(constants.MAX_WORD_LENGHT, )
 	dst.addSample(tuple(netInput))
 	result=net.activateOnDataset(dst)
 

8968f3bf9ee1167644c2efe3ab26a32a5bc50716
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2019-05-18 18:18:44 -0300

    add constants

--- constants.py
+++ constants.py
@@ -4,4 +4,13 @@ FRENCH = 3
 GERMAN = 4
 JAPANESE = 5
 
+dicLanguageIdToString = {SPANISH: 'Spanish',
+						 ENGLISH: 'English',
+						 FRENCH: 'French',
+						 GERMAN: 'German',
+						 JAPANESE: 'Japanese'
+}
+
 MAX_WORD_LENGHT = 20
+OUTPUT_NODES = 1
+HIDDEN_NODES = 10

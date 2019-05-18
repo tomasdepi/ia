81bb01870b0632156af4279562428dc7d434a4ae
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2019-05-18 18:19:04 -0300

    using constants

--- utilities.py
+++ utilities.py
@@ -1,7 +1,9 @@
+import constants
+
 def wordToList(word):
 
 	lenWord = len(word)
-	listOfCeros = [0] * (20-lenWord)
+	listOfCeros = [0] * (constants.MAX_WORD_LENGHT-lenWord)
 	l = [ord(character) for character in word]
 
 	return l + listOfCeros

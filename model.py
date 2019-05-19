05cf27fac2d4214ab99e043768ad088613019470
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2019-05-18 23:40:52 -0300

    updates

--- model.py
+++ model.py
@@ -8,18 +8,22 @@ from pybrain.datasets import UnsupervisedDataSet
 from pybrain.structure.modules import TanhLayer
 import sys
 
+testName = str(sys.argv[1])
+
 print('[+] Loading data set')
 
 dataset = SupervisedDataSet(constants.MAX_WORD_LENGHT, constants.OUTPUT_NODES)
 
-dataframe = pd.read_csv('spanish_german_sample.csv')
+dataframe = pd.read_csv('{testName}.csv'.format(testName=testName))
 dataframeTraining = dataframe.drop(columns=['Word', 'Len'])
+
 
 dataframeLen = len(dataframe)
 lenForTesting = round(dataframeLen * 0.25)
 
 dataframeTesting = dataframeTraining.sample(lenForTesting)
 dataframeTraining = dataframeTraining[~dataframeTraining.index.isin(list(dataframeTesting.index))]
+dataframeTraining.to_csv("training_{testName}.csv".format(testName=testName), index=False)
 
 for _, row in dataframeTraining.iterrows():
 	
@@ -34,9 +38,9 @@ print('[+] Starting Tranining.....')
 
 net = buildNetwork(constants.MAX_WORD_LENGHT, constants.HIDDEN_NODES, constants.OUTPUT_NODES, bias=True) # cant input nodes, hidden nodes nd output nodes
 trainer = BackpropTrainer(net, dataset, learningrate = 0.05)
-#trainer.trainOnDataset(dataset, 10)
-#trainer.testOnData(verbose=True)
-trainer.train()
+trainer.trainOnDataset(dataset, 10)
+trainer.testOnData(verbose=True)
+#trainer.train()
 
 print("[+] The Training has finished")
 
@@ -45,11 +49,12 @@ languageIds = []
 languages = []
 results = []
 errors = []
-for rowId, row in dataframeTraining.iterrows():
+inputs = [] 
+for rowId, row in dataframeTesting.iterrows():
 
 	output = tuple([row["Language"]])
-	inputs = tuple(row[1:]) # remove language, keep only the 20 characters values	
-	result = net.activate(inputs)[0].round(5)
+	inputNet = tuple(row[1:]) # remove language, keep only the 20 characters values	
+	result = net.activate(inputNet)[0].round(5)
 	languageId = dataframe.at[rowId, "Language"]
 	error = (result - languageId).round(5)
 	word = dataframe.at[rowId, "Word"]
@@ -59,23 +64,8 @@ for rowId, row in dataframeTraining.iterrows():
 	languages.append(constants.dicLanguageIdToString[languageId])
 	results.append(result)
 	errors.append(error)
+	inputs.append(inputNet)
 
-dfSaveOutput = pd.DataFrame(data={'Word':words, 'Expected Language Id': languageIds, 'Expected Language': languages, 'Result': results, 'Error': errors})
-dfSaveOutput.to_csv('result_testing.csv', index=False)
-'''
+dfSaveOutput = pd.DataFrame(data={'Word':words, "Input":inputs, 'Expected Language Id': languageIds, 'Expected Language': languages, 'Result': results, 'Error': errors})
+dfSaveOutput.to_csv('result_{testName}.csv'.format(testName=testName), index=False)
 
-while 1:
-
-	userInput = input("Input Word:")
-
-	if len(userInput) > constants.MAX_WORD_LENGHT:
-		print("[-] Error: The word exced {maxCharacters} characters".format(maxCharacters=constants.MAX_WORD_LENGHT))
-		continue
-
-	netInput = utilities.wordToList(userInput)
-	
-	result=net.activate(netInput)
-
-	print(result)
-
-'''
